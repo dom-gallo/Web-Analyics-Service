734f418719386f22e40e4bcf5e2bf3576a938dbb
Dominic Gallo <dgallo519@protonmail.com>
2022-05-14 09:14:01 -0400

    testing heroku deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dspring.profiles.active=prod -Dserver.port=$PORT -jar target/analytics_server_dev-0.0.1-SNAPSHOT.jar
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,5 +1,4 @@
 const currentLocation = window.location;
-
 const Http = new XMLHttpRequest();
 const url =`http://localhost:8080/api/v1/pageview/new?url=${currentLocation}`
 Http.open("GET", url);
@@ -7,3 +6,14 @@ Http.send();
 Http.onreadystatechange = (e) => {
     console.log(Http.responseText);
 };
+
+[
+    {"id":32,"viewedAt":"2022-05-06T19:13:31.936023","url_viewed":"/hi"},
+    {"id":33,"viewedAt":"2022-05-06T19:13:34.092667","url_viewed":"/hi1"},
+    {"id":34,"viewedAt":"2022-05-06T19:13:35.13127","url_viewed":"/hi12"},
+    {"id":35,"viewedAt":"2022-05-06T19:13:35.998821","url_viewed":"/hi"},
+    {"id":36,"viewedAt":"2022-05-06T19:13:36.921716","url_viewed":"/hi"},
+    {"id":37,"viewedAt":"2022-05-06T19:13:38.278002","url_viewed":"/"},
+    {"id":38,"viewedAt":"2022-05-06T19:13:41.347037","url_viewed":"/home"},
+    {"id":39,"viewedAt":"2022-05-06T19:13:44.451061","url_viewed":"/home/hello"}
+]
